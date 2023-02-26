87775805fd79f2d1a90c7519ba21336903226c04
Kun Zhang <kzgoldenwing@gmail.com>
2023-02-26 02:05:03 -0500

    Add files via upload

---
+++ src/main/java/com/everyday/erm/ErmApplication.java
@@ -0,0 +1,13 @@
+package com.everyday.erm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmApplication.class, args);
+	}
+
+}
